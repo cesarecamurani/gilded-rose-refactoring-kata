4028348e058b91af10da39d643582a32d6228bd0
cesare camurani <cesare.camurani@gmail.com>
2018-12-09 11:07:29 +0000

    Increase - Decrease quality methods added

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,21 +9,21 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            decrease_quality(item)
           end
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          increase_quality(item)
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality(item)
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality(item)
               end
             end
           end
@@ -37,7 +37,7 @@ class GildedRose
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                decrease_quality(item)
               end
             end
           else
@@ -51,4 +51,12 @@ class GildedRose
       end
     end
   end
+end
+
+def decrease_quality(item)
+  item.quality -= 1
+end
+
+def increase_quality(item)
+  item.quality += 1
 end
