700768e68d56eb63e13449e081721cea256d6c68
cesare camurani <cesare.camurani@gmail.com>
2018-12-09 18:20:54 +0000

    Trying another approach - Passing test one at a time

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,92 +1,74 @@
 class GildedRose
+  AGED_BRIE = "Aged Brie"
+  BACKSTAGE_PASS = "Backstage pass for a Metallica concert"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+  MIN_QUALITY = 0
+  MAX_QUALITY = 50
 
   def initialize(items)
     @items = items
   end
 
-  def update_quality
-    @items.each do |item|
-
-      if !aged_brie(item) && !backstage(item) && item.quality > 0
-
-          if !sulfuras(item)
-            decrease_quality(item)
-          end
-
-
-      else
-
-        if item.quality < 50
-          increase_quality(item)
-
-          if backstage(item)
-
-            if item.sell_in < 11 && item.quality < 50
-              increase_quality(item)
-            end
-
-            if item.sell_in < 6 && item.quality < 50
-              increase_quality(item)
-            end
-
-          end
-
-        end
-      end
-
-      if !sulfuras(item)
-        decrease_sell_in(item)
-      end
-
-      if expired(item)
-        if !aged_brie(item)
-          if !backstage(item)
-            if item.quality > 0
-              if !sulfuras(item)
-                decrease_quality(item)
-              end
-            end
-
-          else
-            item.quality -= item.quality
-          end
-
-        else
-          if item.quality < 50
-            increase_quality(item)
-          end
-        end
-
-      end
+  def update_generic_item_quality(item)
+    if item.quality > MIN_QUALITY
+      expired(item) ? item.quality -= 2 : item.quality -= 1
     end
   end
-end
 
+  def update_aged_brie(item)
+    if item.quality < MAX_QUALITY
+      expired(item) ? item.quality += 2 : item.quality += 1
+    end
+  end
 
-def decrease_quality(item)
-  item.quality -= 1
-end
+  def expired(item)
+    item.sell_in <= 0
+  end
 
-def increase_quality(item)
-  item.quality += 1
-end
-
-def decrease_sell_in(item)
-  item.sell_in -= 1
-end
-
-def expired(item)
-  item.sell_in < 0
-end
-
-def aged_brie(item)
-  item.name == "Aged Brie"
-end
-
-def backstage(item)
-  item.name == "Backstage pass for a Metallica concert"
-end
-
-def sulfuras(item)
-  item.name == "Sulfuras, Hand of Ragnaros"
+  #   @items.each do |item|
+  #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #       if item.quality > 0
+  #         if item.name != "Sulfuras, Hand of Ragnaros"
+  #           item.quality = item.quality - 1
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50
+  #         item.quality = item.quality + 1
+  #         if item.name == "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.sell_in < 11
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #           if item.sell_in < 6
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #     if item.name != "Sulfuras, Hand of Ragnaros"
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #     if item.sell_in < 0
+  #       if item.name != "Aged Brie"
+  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.quality > 0
+  #             if item.name != "Sulfuras, Hand of Ragnaros"
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           item.quality = item.quality - item.quality
+  #         end
+  #       else
+  #         if item.quality < 50
+  #           item.quality = item.quality + 1
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
