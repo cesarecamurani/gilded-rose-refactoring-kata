9e142242c4e3c5d8044932f97d40c7188e0fb23a
cesare camurani <cesare.camurani@gmail.com>
2018-12-10 22:40:30 +0000

    Cleaned a bit - got rid of a couple of methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -13,77 +13,70 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       lower_sell_in(item)
-      check_generic_quality(item)
-      check_if_backstage(item)
-      check_is_aged_brie(item)
+      update_generic_item(item)
+      update_backstage_pass(item)
+      update_aged_brie(item)
     end
-  end
-
-
-  def expired(item)
-    item.sell_in < 0
-  end
-
-  def is_backstage_pass?(item)
-    item.name == BACKSTAGE_PASS ? set_quality_to_zero(item) : reached_min_quality?(item)
-  end
-
-  def check_if_backstage(item)
-    increase_if_ten_to_six(item) && increase_if_five_to_zero(item) if item.name == BACKSTAGE_PASS
-  end
-
-  def check_is_aged_brie(item)
-    (item.name == AGED_BRIE ? reached_max_quality?(item) : is_backstage_pass?(item)) if expired(item)
   end
 
   def lower_sell_in(item)
     item.name == SULFURAS ? item.sell_in : item.sell_in -= 1
   end
 
-  def check_generic_quality(item)
-    generic_item(item) ? reached_min_quality?(item) : reached_max_quality?(item)
+  def update_generic_item(item)
+    generic_item(item) ? above_min_quality?(item) : below_max_quality?(item)
   end
 
-
-
-  def lower_quality(item)
-    item.name == SULFURAS ? item.quality : decrease_quality(item)
+  def update_backstage_pass(item)
+    (increase_if_ten_to_six(item) && increase_if_five_to_zero(item)) if item.name == BACKSTAGE_PASS
   end
 
-  def reached_min_quality?(item)
+  def update_aged_brie(item)
+    (item.name == AGED_BRIE ? below_max_quality?(item) : is_backstage_pass?(item)) if expired(item)
+  end
+
+  def expired(item)
+    item.sell_in < 0
+  end
+
+  def is_backstage_pass?(item)
+    item.name == BACKSTAGE_PASS ? set_quality_to_zero(item) : above_min_quality?(item)
+  end
+
+  def above_min_quality?(item)
     item.quality > MIN_QUALITY ? lower_quality(item) : item.quality
   end
 
-  def reached_max_quality?(item)
+  def below_max_quality?(item)
     item.quality < MAX_QUALITY ? increase_quality(item) : item.quality
+  end
+
+  def lower_quality(item)
+    item.name == SULFURAS ? item.quality : item.quality -= 1
   end
 
   def increase_quality(item)
     item.quality += 1
   end
 
-  def decrease_quality(item)
-    item.quality -= 1
-  end
-
   def set_quality_to_zero(item)
     item.quality -= item.quality
   end
 
-  def backstage_quality_less_than_ten(item)
+  def backstage_sell_in_less_than_ten(item)
     item.sell_in <= 10 && item.quality < MAX_QUALITY
   end
 
-  def backstage_quality_less_than_five(item)
+  def backstage_sell_in_less_than_five(item)
     item.sell_in <= 5 && item.quality < MAX_QUALITY
   end
 
   def increase_if_ten_to_six(item)
-    backstage_quality_less_than_ten(item) ? increase_quality(item) : item.quality
+    backstage_sell_in_less_than_ten(item) ? increase_quality(item) : item.quality
   end
 
   def increase_if_five_to_zero(item)
-    backstage_quality_less_than_five(item) ? increase_quality(item) : item.quality
+    backstage_sell_in_less_than_five(item) ? increase_quality(item) : item.quality
   end
 
   def generic_item(item)
